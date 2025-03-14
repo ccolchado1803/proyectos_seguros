71fd9b9c37b10225100b4130fde9af31a4d4fc45
Claudia Colchado <claudya1803@gmail.com>
2025-03-13 21:37:30 -0500

    segunda version del componente de transformacion

--- data_transform.py
+++ data_transform.py
@@ -10,6 +10,10 @@ try:
     # Ordenar los datos por nombre
     data_ordenada = data.sort_values(by='Nombre1')
     
+    # regla 2: transformar datos null por cero
+    # regla 3: eliminar el tercer nombre de cada registro
+
+
     # Exportar a Excel
     archivo_excel = 'clientes_ordenados.xlsx'
     data_ordenada.to_excel(archivo_excel, index=False)
